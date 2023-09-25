3eb302781b0fdaf02bfbf9783d72f4278bf3a273
Harshit Vashisht <120767685+HarshitVashisht11@users.noreply.github.com>
2023-09-25 20:38:12 +0530

    Update firebase.js

--- my-project/src/sections/firebase.js
+++ my-project/src/sections/firebase.js
@@ -4,14 +4,14 @@ import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyCHHjGsEvhqdZjGhwE4i7_hSLn7ClvGw74",
-  authDomain: "transport-hack-10b1b.firebaseapp.com",
-  databaseURL: "https://transport-hack-10b1b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "transport-hack-10b1b",
-  storageBucket: "transport-hack-10b1b.appspot.com",
-  messagingSenderId: "74418803669",
-  appId: "1:74418803669:web:dc357939daa27794f1a35c",
-  measurementId: "G-JB9B4KNLR3"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
